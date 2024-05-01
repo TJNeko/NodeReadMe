83fdca0c2f1b9f3d39f8eb48f09ffa5e0dda3626
Nekoteej <161190070+Nekoteej@users.noreply.github.com>
2024-05-01 04:16:56 -0600

    added other questions and other template readme things, clickable scroll links in table of content as well

--- Develop/index.js
+++ Develop/index.js
@@ -7,7 +7,48 @@ const questions = [
         type: 'input',
         name: 'title',
         message: 'What is the title of the project?',
-    }
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'What is the decription of the project?',
+      },
+      {
+        type: 'input',
+        name: 'installation',
+        message: 'What are the projects installation instructions?',
+      },
+      {
+        type: 'input',
+        name: 'usage',
+        message: 'What is the usage information?',
+      },
+      {
+        type: 'input',
+        name: 'contribution',
+        message: 'What are the contribution guidelines?',
+      },
+      {
+        type: 'input',
+        name: 'tests',
+        message: 'What are the test instructions?',
+      },
+      {
+        type: 'list',
+        name: 'license',
+        message: 'Please choose a license',
+        choices: ['MIT', 'GNU GPLv3', 'Apache License 2.0', 'None'],
+      },
+      {
+        type: 'input',
+        name: 'github',
+        message: 'What is your github username?',
+      },
+      {
+        type: 'input',
+        name: 'email',
+        message: 'What is your email?',
+      },
 ];
 
 // TODO: Create a function to write README file
@@ -15,6 +56,36 @@ function writeToFile(fileName, data) {
     const { title, description, installation, usage, license, contribution, tests, github, email } = data;
     const content = `
 # ${title}
+
+## Description
+${description}
+
+## Table of Contents
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+
+## Installation
+${installation}
+
+## Usage
+${usage}
+
+## License
+This application is covered under the ${license} license.
+
+## Contributing
+${contribution}
+
+## Tests
+${tests}
+
+## Questions
+For additional questions, contact me via GitHub: [${github}](https://github.com/${github})  
+or email: ${email}
     `;
 
     fs.writeFile(fileName, content, (err) => {
